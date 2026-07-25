acd6a7984f33c9ea988a226599eca816a5839b66
dunglex <dung.lex@homecredit.vn>
2026-07-25 13:37:43 +0700

    Add curl command to download gold prices from mihong.vn
    Added a new curl command to download gold prices from mihong.vn and save to a JSON file.

--- download.sh
+++ download.sh
@@ -26,3 +26,23 @@ curl 'https://data-asg.goldprice.org/dbXRates/USD' \
   -H 'referer: https://goldprice.org/' \
   -H 'sec-ch-ua: "Not;A=Brand";v="8", "Chromium";v="150", "Google Chrome";v="150"' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36' > gold_prices_goldprice.json
+
+# Download gold prices from mihong.vn
+curl 'https://api.mihong.vn/v1/gold-prices?market=domestic' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'dnt: 1' \
+  -H 'origin: https://www.mihong.vn' \
+  -H 'pragma: no-cache' \
+  -H 'priority: u=1, i' \
+  -H 'referer: https://www.mihong.vn/' \
+  -H 'sec-ch-ua: "Not;A=Brand";v="8", "Chromium";v="150", "Google Chrome";v="150"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-site' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36' \
+  -H 'x-market: mihong' > gold_prices_mihong.json
